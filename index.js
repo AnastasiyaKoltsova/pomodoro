bb138c3e7803a86c96d2e5e4a7cb08fda98e2f88
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-06-15 22:57:52 +0300

    button toggle

--- index.js
+++ index.js
@@ -3,19 +3,73 @@ const timerDisplay = document.querySelector('.timer');
 const pomodoroButton = document.querySelector('.btn-pomodoro');
 const shortBreakButton = document.querySelector('.short-break');
 const longBreakButton = document.querySelector('.long-break');
+const pomodoroTime = 1500;
+const shortBreakTime = 300;
+const longBreakTime = 900;
+const colors = {
+    pomodoro: {
+        backgroundColor: '#ba4849',
+        pomodoroBackgroundColor: '#c25c5c',
+        switchColor: '#ba4849',
+        pomodoroButtonColor: '#a95250',
+        shortBreakButtonColor: 'transparent',
+        longBreakButtonColor: 'transparent'
+    },
+    short: {
+        backgroundColor: '#38858a',
+        pomodoroBackgroundColor: '#519396',
+        switchColor: '#38858a',
+        pomodoroButtonColor: 'transparent',
+        shortBreakButtonColor: '#467f82',
+        longBreakButtonColor: 'transparent'
+    },
+    long: {
+        backgroundColor: '#397097',
+        pomodoroBackgroundColor: '#5280a2',
+        switchColor: '#397097',
+        pomodoroButtonColor: 'transparent',
+        shortBreakButtonColor: 'transparent',
+        longBreakButtonColor: '#466e8d'
+    }
+};
+
 let timeInSeconds = 1500;
 let timer;
+let currentPomodoroCount = 0;
+let maxPomodoroCount = 3;
+let statusOfTimer = 'pomodoro';
+
+function getReadableTime(time) {
+    const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+    const seconds = (time % 60).toString().padStart(2, '0');
+    return minutes + ':' + seconds;
+}
 
 function startTimer() {
     timer = setInterval(function() {
-        const minutes = Math.floor(timeInSeconds / 60).toString().padStart(2, '0');
-        const seconds = (timeInSeconds % 60).toString().padStart(2, '0');
-        timerDisplay.innerHTML = `${minutes}:${seconds}`;
+        timerDisplay.innerHTML = getReadableTime(timeInSeconds);
         timeInSeconds--;
         if (timeInSeconds < 0) {
             clearInterval(timer);
+            if (statusOfTimer === 'pomodoro') {
+                currentPomodoroCount ++;
+                if (currentPomodoroCount === maxPomodoroCount) {
+                    startLongBreak();
+                    statusOfTimer = 'long';
+                } else {
+                    startShortBreak();
+                    statusOfTimer = 'short';
+                }
+            } else {
+                startPomodoro();
+                statusOfTimer = 'pomodoro';
+            }
         }
-    }, 1000);
+    }, 10);
+}
+
+function updateTimerDisplay(time) {
+    timerDisplay.innerHTML = getReadableTime(time);
 }
 
 function pauseTimer() {
@@ -23,8 +77,23 @@ function pauseTimer() {
 }
 
 function resetTimer() {
-    timeInSeconds = 1500;
-    timerDisplay.innerHTML = '25:00';
+    timeInSeconds = pomodoroTime;
+    updateTimerDisplay(pomodoroTime);
+}
+
+function startTimerWithColor(time, status) {
+    resetTimer();
+    applyColors(
+        colors[status].backgroundColor,
+        colors[status].pomodoroBackgroundColor,
+        colors[status].switchColor,
+        colors[status].pomodoroButtonColor,
+        colors[status].shortBreakButtonColor,
+        colors[status].longBreakButtonColor
+    );
+    timeInSeconds = time;
+    updateTimerDisplay(time);
+    statusOfTimer = status;
 }
 
 function applyColors(backgroundColor, pomodoroBackgroundColor, switchColor, pomodoroButtonColor, shortBreakButtonColor, longBreakButtonColor) {
@@ -37,25 +106,18 @@ function applyColors(backgroundColor, pomodoroBackgroundColor, switchColor, pomo
 }
 
 function startShortBreak() {
-    resetTimer();
-    applyColors('#38858a', '#519396', '#38858a', 'transparent', '#467f82', 'transparent');
-    timeInSeconds = 300;
-    timerDisplay.innerHTML = '05:00';
+    startTimerWithColor(shortBreakTime, 'short');
 }
 
 function startLongBreak() {
-    resetTimer();
-    applyColors('#397097', '#5280a2', '#397097', 'transparent', 'transparent', '#466e8d');
-    timeInSeconds = 900;
-    timerDisplay.innerHTML = '15:00';
+    startTimerWithColor(longBreakTime, 'long');
 }
 
 function startPomodoro() {
-    resetTimer();
-    applyColors('#ba4849', '#c25c5c', '#ba4849', '#a95250', 'transparent', 'transparent');
+    startTimerWithColor(pomodoroTime, 'pomodoro');
 }
 
-startButton.addEventListener('click', function() {
+function toggleStartPauseButton() {
     if (startButton.innerHTML === 'START') {
         startButton.innerHTML = 'PAUSE';
         startTimer();
@@ -63,22 +125,28 @@ startButton.addEventListener('click', function() {
         startButton.innerHTML = 'START';
         pauseTimer();
     }
-});
+}
+
+startButton.addEventListener('click', toggleStartPauseButton);
 
 shortBreakButton.addEventListener('click', function() {
     startButton.innerHTML = 'START';
     pauseTimer();
-    startShortBreak();
+    startTimerWithColor(shortBreakTime, 'short');
 });
 
 pomodoroButton.addEventListener('click', function() {
     startButton.innerHTML = 'START';
     pauseTimer();
-    startPomodoro();
+    startTimerWithColor(pomodoroTime, 'pomodoro');
 });
 
 longBreakButton.addEventListener('click', function() {
     startButton.innerHTML = 'START';
     pauseTimer();
-    startLongBreak();
+    startTimerWithColor(longBreakTime, 'long');
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    timerDisplay.innerHTML = getReadableTime(pomodoroTime);
 });
