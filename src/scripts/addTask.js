ad7b3e4821684916d0c78b6e86d111bbaa75d30e
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-06-20 13:31:54 +0300

    added eventlisteners for buttons

--- src/scripts/addTask.js
+++ src/scripts/addTask.js
@@ -7,61 +7,34 @@ const inputTask = document.querySelector('.tasks-description__settings-input-tas
 const inputNumber = document.querySelector('.tasks-description__settings-input-number');
 const saveBtn = document.querySelector('.tasks-save');
 
-addTaskBtn.addEventListener('click', function() {
-    setTaskWindow.classList.remove('hidden');
-    addTaskBtn.classList.add('.hidden');
-});
+function getSavedTasks() {
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+}
 
-cancelBtn.addEventListener('click', function() {
-    setTaskWindow.classList.add('hidden');
-    addTaskBtn.classList.remove('.hidden');
-});
+function saveTasks(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
-saveBtn.addEventListener('click', function() {
-    const task = inputTask.value.trim();
-    if (task === '') {
-        return;
-    }
-    const count = inputNumber.value || 1;
-    
-    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    savedTasks.push({ task, count });
-    localStorage.setItem('tasks', JSON.stringify(savedTasks));
-    
+function createTaskElement(task, count) {
     const newTask = document.createElement('div');
     newTask.classList.add('tasks-list__item');
     newTask.innerHTML = `
-        <div class="tasks-list__item-color"></div>
-        <img class="tasks-list__item-icon" src="./src/assets/icons/check.png">
-        <div class="tasks-list__item-task">${task}</div>
-        <div class="tasks-list__item-count">0/${count}</div>
-        <button class="tasks-list__item-btn">
-            <img src="./src/assets/icons/dots.png" alt="More options" class="dots"></img>
-        </button>
-    `;
-    tasksList.appendChild(newTask);
-    
-    tasksList.classList.remove('hidden');
-    
-    inputTask.value = '';
-    inputNumber.value = '';
-    setTaskWindow.classList.add('hidden');
-});
+<div class="tasks-list__item-color"></div>
+<img class="tasks-list__item-icon" src="./src/assets/icons/check.png">
+<div class="tasks-list__item-task">${task}</div>
+<div class="tasks-list__item-count">0/${count}</div>
+<button class="tasks-list__item-btn">
+    <img src="./src/assets/icons/dots.png" alt="More options" class="dots"></img>
+</button>
+`;
+
+    return newTask;
+}
 
 function loadTasks() {
-    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const savedTasks = getSavedTasks();
     savedTasks.forEach(({ task, count }) => {
-        const newTask = document.createElement('div');
-        newTask.classList.add('tasks-list__item');
-        newTask.innerHTML = `
-            <div class="tasks-list__item-color"></div>
-            <img class="tasks-list__item-icon" src="./src/assets/icons/check.png">
-            <div class="tasks-list__item-task">${task}</div>
-            <div class="tasks-list__item-count">0/${count}</div>
-            <button class="tasks-list__item-btn">
-                <img src="./src/assets/icons/dots.png" alt="More options" class="dots"></img>
-            </button>
-        `;
+        const newTask = createTaskElement(task, count);
         tasksList.appendChild(newTask);
     });
 
@@ -69,4 +42,38 @@ function loadTasks() {
         tasksList.classList.remove('hidden');
     }
 }
+
 loadTasks();
+
+setTaskWindow.classList.add('hidden'); 
+
+addTaskBtn.addEventListener('click', function() { 
+    setTaskWindow.classList.remove('hidden'); 
+    addTaskBtn.classList.add('hidden'); 
+}); 
+
+cancelBtn.addEventListener('click', function() { 
+    setTaskWindow.classList.add('hidden'); 
+    addTaskBtn.classList.remove('hidden'); 
+}); 
+
+saveBtn.addEventListener('click', function() { 
+    const task = inputTask.value.trim(); 
+    if (!task) return; 
+    const count = inputNumber.value || 1; 
+    
+    const savedTasks = getSavedTasks(); 
+    savedTasks.push({ task, count }); 
+    saveTasks(savedTasks); 
+
+    const newTask = createTaskElement(task, count); 
+    tasksList.appendChild(newTask); 
+    
+    tasksList.classList.remove('hidden'); 
+    
+    inputTask.value = ''; 
+    inputNumber.value = ''; 
+    setTaskWindow.classList.add('hidden'); 
+    
+    addTaskBtn.classList.remove('hidden');
+});
