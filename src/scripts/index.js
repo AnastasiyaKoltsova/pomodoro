2bbd600db9540240ef38f68a1275c007687f4186
Anastasiya Koltsova <Koltsova_av@mail.ru>
2023-06-23 18:00:32 +0300

    colors.js has been deleted

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,3 @@
-import colors from "/src/scripts/colors.js";
-
 const startButton = document.querySelector('.switch');
 const timerDisplay = document.querySelector('.timer');
 const pomodoroButton = document.querySelector('.btn-pomodoro');
@@ -7,6 +5,48 @@ const shortBreakButton = document.querySelector('.short-break');
 const longBreakButton = document.querySelector('.long-break');
 const resetButton = document.querySelector('.reset-btn');
 const addTaskBtn = document.querySelector('.tasks-add__btn');
+
+const colors = {
+    pomodoro: {
+        backgroundColor: '#ba4849',
+        pomodoroBackgroundColor: '#c25c5c',
+        switchColor: '#ba4849',
+        pomodoroButtonColor: '#a95250',
+        shortBreakButtonColor: 'transparent',
+        longBreakButtonColor: 'transparent',
+        addTaskBtnBackground: '#ad4343',
+        addTaskBtnBorder: '#ca8685',
+        addTaskBtnText: '#e6c2c1',
+        addTaskLine: '#e5bcbb',
+        addTaskBtnSetting: '#c86e6e'
+    },
+    short: {
+        backgroundColor: '#38858a',
+        pomodoroBackgroundColor: '#519396',
+        switchColor: '#38858a',
+        pomodoroButtonColor: 'transparent',
+        shortBreakButtonColor: '#467f82',
+        longBreakButtonColor: 'transparent',
+        addTaskBtnBackground: '#347b80',
+        addTaskBtnBorder: '#7fa8ab',
+        addTaskBtnText: '#bed4d5',
+        addTaskLine: '#b8d0d2',
+        addTaskBtnSetting: '#639ca0'
+    },
+    long: {
+        backgroundColor: '#397097',
+        pomodoroBackgroundColor: '#5280a2',
+        switchColor: '#397097',
+        pomodoroButtonColor: 'transparent',
+        shortBreakButtonColor: 'transparent',
+        longBreakButtonColor: '#466e8d',
+        addTaskBtnBackground: '#34688c',
+        addTaskBtnBorder: '#7d9bb3',
+        addTaskBtnText: '#bacad7',
+        addTaskLine: '#b7c8d7',
+        addTaskBtnSetting: '#638caa' 
+    }
+};
 
 const pomodoroTime = 1500;
 const shortBreakTime = 300;
